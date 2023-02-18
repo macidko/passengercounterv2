4209916fb688fe08904071a03195ec48e503d5ea
Eren Macit <ernmctt@gmail.com>
2023-02-18 19:16:34 +0300

    odev12

--- ödev12.js
+++ ödev12.js
@@ -1,11 +1,17 @@
 let saveEl = document.getElementById("save-el")
+let totalEl = document.getElementById("total-el")
 let gecmisBilgi = localStorage.getItem("gecmisBilgi")
+let totalCount = null
+
 if( gecmisBilgi !== null) {
     saveEl.textContent = localStorage.getItem("gecmisBilgi")
+    totalEl.textContent = localStorage.getItem("totalPeople")
+    totalCount = Number(localStorage.getItem("totalPeople"))
+
 } else {
     saveEl.textContent = ""
+    totalEl.textContent = 0
 }
-
 
 let countEl = document.getElementById("count-el")
 let count = 0
@@ -19,6 +25,12 @@ function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
     localStorage.setItem( "gecmisBilgi", saveEl.textContent )
+
+    totalCount += count
+    totalEl.textContent = totalCount
+    localStorage.setItem("totalPeople", totalEl.textContent)
+
     countEl.textContent = 0
     count = 0
+
 }
